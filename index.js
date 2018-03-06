3672d657b9641592298ca74536f1fc3f0dad8c5c
Justin Chau <chausicle@gmail.com>
2018-03-06 10:55:21 -0800

    Add get events

--- index.js
+++ index.js
@@ -2,5 +2,19 @@ const express = require('express')
 const port = 3000
 const app = express()
 
+let events = [
+  {
+    uid: 1,
+    title: 'Birthday Party',
+    description: 'First birthday party',
+    date: 'July 8th',
+    time: '10pm-10am'
+  }
+]
+
+app.get('/events', (req, res) => {
+  console.log('GET is working!')
+  res.send(events)
+})
 
 app.listen(port, () => console.log('HELLOOOOOO'))
